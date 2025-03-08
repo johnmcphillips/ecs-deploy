bf7c1763b75b915b66f56303ab34031db5ce3819
johnmcphillips <john.mcphillips@gmail.com>
2025-03-08 17:32:01 -0600

    sg

--- main.tf
+++ main.tf
@@ -24,17 +24,18 @@ resource "aws_security_group" "ECS_SG" {
 
 resource "aws_security_group" "ALB_SG" {
   vpc_id = aws_vpc.main.id
+  
   ingress {
     from_port = 80
     to_port   = 80
     protocol = "tcp"
-    security_groups = ["0.0.0.0/0"]
+    cidr_blocks = ["0.0.0.0/0"]
  }
  ingress {
     from_port = 443
     to_port   = 443
     protocol = "tcp"
-    security_groups = ["0.0.0.0/0"]
+    cidr_blocks = ["0.0.0.0/0"]
  }
   egress {
     from_port = 0
