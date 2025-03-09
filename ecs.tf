a516d22dfad683e6fc37e0078b510238c4969db3
johnmcphillips <john.mcphillips@gmail.com>
2025-03-08 19:08:14 -0600

    log group

--- ecs.tf
+++ ecs.tf
@@ -53,7 +53,7 @@ resource "aws_ecs_task_definition" "ECS_Task_Definition" {
     logConfiguration = {
       logDriver = "awslogs"
       options = {
-        "awslogs-group"         = "aws_cloudwatch_log_group.ECS_Log_Group.name"
+        "awslogs-group"         = "/ecs/${var.ecs_task_name}"
         "awslogs-region"        = var.region
         "awslogs-stream-prefix" = "ecs"
       }
